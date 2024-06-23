b959467ed7a5d96504b4eded1fef00aae0cca6aa
Asahi Komori <v23e3012@oita-u.ac.jp>
2024-06-23 14:28:19 +0000

    Form関連ヘルパーとCSRF対策

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,25 @@ class PostsController < ApplicationController
         @posts = Post.all.order(created_at: 'desc')
     end
     
+    def show
+        @post = Post.find(params[:id])
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render "new", status: :unprocessable_entity
+        end
+    end
+    
+    private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
