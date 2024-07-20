f296b2b6d47d9e781b842ef3daa8b808bc946058
Asahi Komori <v23e3012@oita-u.ac.jp>
2024-07-20 08:22:35 +0000

    ユーザ認証機能途中まで2

---
+++ app/controllers/users_controler.rb
@@ -0,0 +1,17 @@
+def new
+    @user = User.new
+end
+
+def create
+    @user = User.new(user.params)
+    if @user.save
+        redirect_to posts_path
+    else
+        render 'new' , status: :unprocessable_entity
+    end
+end
+
+private
+def user_params
+    params.require(:user).permit(:signin_name, :display_name, :password, :password_confirmation)
+end
